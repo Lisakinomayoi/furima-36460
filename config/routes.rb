411dc5ee75d5febd681f18af38c5c4d245740f14
Lisakinomayoi <cynthianishimura0501@gmail.com>
2021-10-23 02:59:02 +0900

    新規登録機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'items#index'
   resources :users, only: [:new, :create]
+  root to: 'items#index'
   resources :items, only: [:index]
   
 end
