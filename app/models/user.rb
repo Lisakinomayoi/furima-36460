b8583c2b3a3eb8dd4d1dc6647cf9d32aba33f4a0
Lisakinomayoi <cynthianishimura0501@gmail.com>
2021-10-27 06:39:04 +0900

    エラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   with_options presence: true do
-    validates :nickname
+    validates :nickname, presence: {message: 'nickname cant be black'}
     validates :email
     validates :encrypted_password #format: {with:#/(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/}#{with: /\A[a-z0-9]+\z/, message: 'Half-width characters numbers'}
     validates :last_name, presence: {message: 'last name cant be black'}, format: {with: /[^ -~｡-ﾟ]+/,message: 'Full-width characters'}
-    validates :first_name, format: {with: /[^ -~｡-ﾟ]+/,message: 'Full-width characters'}
-    validates :last_name_kana, format: {with: /[^ -~｡-ﾟ]+/,message: 'Full-width characters'}
-    validates :first_name_kana, format: {with: /[^ -~｡-ﾟ]+/,message: 'Full-width characters'}
+    validates :first_name, presence: {message: 'first name cant be black'},format: {with: /[^ -~｡-ﾟ]+/,message: 'Full-width characters'}
+    validates :last_name_kana, presence: {message: 'last name kana cant be black'},format: {with: /[^ -~｡-ﾟ]+/,message: 'Full-width characters'}
+    validates :first_name_kana, presence: {message: 'first name kana cant be black'},format: {with: /[^ -~｡-ﾟ]+/,message: 'Full-width characters'}
     validates :birth_date
   end
-  validates :encrypted_password,:password,:password_confirmation,length:{minimum:6},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
+  validates :encrypted_password,:password,:password_confirmation,presence: {message: 'password cant be black'},length:{minimum:6},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
   #VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i#/A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
   #validates :encrypted_password, confirmation: true, format: { with: VALID_PASSWORD_REGEX,
    # message: "は半角6文字以上英大文字・小文字・数字それぞれ１文字以上含む必要があります"}
