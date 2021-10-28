af54fb95200b20a3831ecd4bef869ad7947a227a
Lisakinomayoi <cynthianishimura0501@gmail.com>
2021-10-29 00:39:18 +0900

    テーブル、モデルの作成、Active Storageの導入

---
+++ app/models/item.rb
@@ -0,0 +1,20 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :prefecture
+  belongs_to :sales_status
+  belongs_to :scheduled_delivery
+  belongs_to :shipping_fee_status
+  belongs_to :user
+  has_one_attached :image
+  validates :image, :name, :info, :category_id, :prefecture_id, :sales_status_id, :scheduled_delivery_id, :shipping_fee_status_id, :price, :user_id, presence: true
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :sales_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"}    
+  validates :shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :price, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
+  validates :price, numericality: true
+end
+
