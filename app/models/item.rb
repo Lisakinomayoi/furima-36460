eac1e460c292e2bd3af1a75747e578bf6a2903f7
Lisakinomayoi <cynthianishimura0501@gmail.com>
2021-11-05 02:04:46 +0900

    単体テストコードの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  belongs_to :user, optional: true
+  belongs_to :user
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
@@ -18,13 +18,15 @@ class Item < ApplicationRecord
   #   validates :info
   #   validates :image
   # end  
-  validates :name, :info, :category_id, :prefecture_id, :sales_status_id, :shipping_fee_status_id, :price, :user_id, presence: true
+  validates :name, :info, :category_id, :prefecture_id, :sales_status_id, :shipping_fee_status_id, :price, presence: true
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :sales_status_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"}    
   validates :shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :price, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
-  validates :price, numericality: true
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range"}
+  validates :price, numericality: { only_integer: true, message: "Half-width number." }
+  #validates :price, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
+  #validates :price, numericality: true
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    image{Faker::Lorem.sentence}
+    name                   {"名前"}
+    info                   {"説明"}
+    category_id            {2}
+    sales_status_id        {2}
+    shipping_fee_status_id {2}
+    prefecture_id          {2}
+    scheduled_delivery_id  {2}
+    price                  {5678}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
