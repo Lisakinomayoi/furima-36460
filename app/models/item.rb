0eba689575993ad2870db82e360004d53804f57b
Lisakinomayoi <cynthianishimura0501@gmail.com>
2021-11-05 02:57:23 +0900

    画像が必須である

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,7 +18,7 @@ class Item < ApplicationRecord
   #   validates :info
   #   validates :image
   # end  
-  validates :name, :info, :category_id, :prefecture_id, :sales_status_id, :shipping_fee_status_id, :price, presence: true
+  validates :name, :info, :category_id, :prefecture_id, :sales_status_id, :shipping_fee_status_id, :price, :image, presence: true
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
