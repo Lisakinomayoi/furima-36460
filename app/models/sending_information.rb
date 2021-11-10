750c7727a1719da67a0619ae847513f9a4343079
Lisakinomayoi <cynthianishimura0501@gmail.com>
2021-11-10 22:03:41 +0900

    formオブジェクトパターン

--- app/models/sending_information.rb
+++ app/models/sending_information.rb
@@ -1,18 +1,3 @@
-class SendingInformation < ApplicationRecord
-  include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :order
+class SendingInformation
   belongs_to :order
-  validates :prefecture_id, numericality: {other_than: 1 }
-  with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/ }
-    validates :prefecture_id
-    validates :city
-    validates :address
-    validates :phone_number, {with: /\A[0-9]{11}\z/ }
-  end
-  
-  def save
-    purchase = Purchase.create(item_id: item_id, user_id: user_id)
-    SendingInformation.create(postal_code :postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, order: oreder)
-  end
 end
