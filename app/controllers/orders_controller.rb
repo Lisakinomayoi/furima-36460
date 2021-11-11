f296ee276fdf4b79741f501fdf2732b7628778d4
Lisakinomayoi <cynthianishimura0501@gmail.com>
2021-11-12 02:29:01 +0900

    バリデーション修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -38,7 +38,7 @@ class OrdersController < ApplicationController
 
   private 
   def order_params
-    params.permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: @item_id)
-    #params.require(:order_sending_information).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user, item_id: @item.id)
+    #params.permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: @item_id)
+    params.require(:order_sending_information).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user, item_id: @item.id)
   end
 end
