ea915e3e8acc389fee3555ea1373ca1f652a8867
Lisakinomayoi <cynthianishimura0501@gmail.com>
2021-11-12 15:56:24 +0900

    コメントアウト削除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,30 +2,15 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
-    @order = Order.new # (order_params)
+    @order = Order.new 
     @item = Item.find(params[:item_id])
-    @order_sending_information = OrderSendingInformation.new # (order_params)
-    # @order_information = OrderInformation.new
+    @order_sending_information = OrderSendingInformation.new 
   end
 
   def new
     @order_sending_information = OrderSendingInformation.new
-    # @order = Order.new
   end
 
-  # def create
-  #   @order = Order.create(order_params)
-  #   SendingInformation.create(sending_information_params)
-  #   redirect_to root_path
-  # end
-
-  # private
-  # def order_params
-  #   params.permit(:item_id).merge(user_id: current_user.id)
-  # end
-  # def sending_information_params
-  #   params.permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(order_id: @order_id)
-  # end
 
   def create
     @item = Item.find(params[:item_id])
@@ -42,7 +27,6 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    # params.permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: @item_id)
     params.require(:order_sending_information).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(
       user_id: current_user.id, item_id: @item.id, token: params[:token]
     )
@@ -51,9 +35,9 @@ class OrdersController < ApplicationController
   def pay_item
     Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
-      amount: @item.price, # 商品の値段
-      card: order_params[:token], # カードトークン
-      currency: 'jpy'                 # 通貨の種類（日本円）
+      amount: @item.price, 
+      card: order_params[:token], 
+      currency: 'jpy'                 
     )
   end
 end
